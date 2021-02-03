4fb241139fa7034bf6f2bbcb2aa70753a2e2327b
Nils Hoffmann <nils.hoffmann@isas.de>
2021-02-03 17:21:52 +0100

    Added resource filtering exclusions, added exception logging.

--- src/main/java/uk/ac/ebi/lipidhome/service/impl/FAScanSpecieServiceImpl.java
+++ src/main/java/uk/ac/ebi/lipidhome/service/impl/FAScanSpecieServiceImpl.java
@@ -10,6 +10,8 @@ import uk.ac.ebi.lipidhome.service.result.model.SimpleSubSpecie;
 
 import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -70,6 +72,7 @@ public class FAScanSpecieServiceImpl extends LipidService implements FAScanSpeci
 			result = new Result(converter.getLipidObjectList(list));
 		} catch (RuntimeException e) {
 			String errorMessage = e.getMessage(); //"Record with id " + id + " is unavailable.";
+                        Logger.getLogger(CategoryServiceImpl.class.getSimpleName()).log(Level.SEVERE, errorMessage, e);
 			result = new Result(errorMessage);
 		}
 		return result2Response(result);
